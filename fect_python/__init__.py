af5eb8f07001b5d4dfced191d223e269bce541b8
Shijian (Andrew) Liu <lshijian405@gmail.com>
2023-01-06 11:49:18 -0500

    Update __init__.py

--- fect_python/__init__.py
+++ fect_python/__init__.py
@@ -6,4 +6,5 @@ Created on Tue Jan  3 09:59:34 2023
 @author: liushijian
 """
 
-from fect_py.fectfunc import Fect
+from fect_python.fectfunc import Fect
+from fect_python.fectfunc import getCohort, panelview, png_show, savefig
